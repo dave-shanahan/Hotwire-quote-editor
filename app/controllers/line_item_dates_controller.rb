fc347ee87572c30aedc09170c3d184c4093255cc
ds0 <ds0@ds0>
2023-09-21 17:22:45 +0100

    implemented turbo for the update and delete methods on line item date

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -24,9 +24,21 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item_date.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed" }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed" }
     end
   end
 
